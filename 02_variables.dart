ad48a49e1ccc8bfdc318a0a80a2d49c282c0406e
angelmontes <angelmontes963@gmail.com>
2023-10-16 08:26:48 -0600

    variables modificacion

--- 02_variables.dart
+++ 02_variables.dart
@@ -6,12 +6,22 @@ void main()
   final List<String> abilities = ['impostor','no impostor'];
   final sprites = <String>['img.jpg','img2.jpg'];
   
+  /* Dinamyc Dato, cualquier tipo de dato) */
+  
+  dynamic errorMessage = "Hola";
+  errorMessage = true;
+  errorMessage = [1,2,3,4];
+  errorMessage = {1,2,3,4};
+  errorMessage = () => true;
+  errorMessage = null;
+  
   print("""
     $pokemon
     $hp
     $bool
     $abilities
     $sprites
+    $errorMessage
   """);
   
 }
