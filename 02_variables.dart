4241580f190332679fd2b33b0c03a0e3e4759a5c
angelmontes <angelmontes963@gmail.com>
2023-10-16 07:56:24 -0600

    Variables

---
+++ 02_variables.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  final String pokemon = 'Ditto';
+  final int hp = 1000;
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor','no impostor'];
+  final sprites = <String>['img.jpg','img2.jpg'];
+  
+  print("""
+    $pokemon
+    $hp
+    $bool
+    $abilities
+    $sprites
+  """);
+  
+}
